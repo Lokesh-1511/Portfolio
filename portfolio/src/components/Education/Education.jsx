fe56e35cb85ec8dc73e81f8f1051d25414127f38
Lokesh <lokeshnagarajan151105@gmail.com>
2025-07-14 23:54:46 +0530

    improved css for education section

--- portfolio/src/components/Education/Education.jsx
+++ portfolio/src/components/Education/Education.jsx
@@ -4,18 +4,20 @@ function Education() {
   return (
     <div id="education" className="education-container">
         <h2 id="title">Education</h2>
-      <p className="education">
-        <h3>Bachelor of Engineering in Computer Science</h3>  
-        <p>Madras Institute of Technology campus, Anna University, Chennai, TamilNadu</p>
-        <p>2023 – 2027 (Expected)</p>
-        <p>CGPA: 8.3 / 10</p>
-      </p>
-      <p className="education">
-        <h3>Higher Secondary (12th Grade)</h3>
-        <p>Bharathi Vidya Bhavan, Erode,TamilNadu</p>
-        <p>2022 – 2023</p>
-        <p>Percentage: 97%</p>
-      </p>
+        <div className="education-content">
+          <p className="education">
+            <h3>Bachelor of Engineering in Computer Science</h3>  
+            <p>Madras Institute of Technology campus, Anna University, Chennai, TamilNadu</p>
+            <p>2023 – 2027 (Expected)</p>
+            <p>CGPA: 8.3 / 10</p>
+          </p>
+          <p className="education">
+            <h3>Higher Secondary (12th Grade)</h3>
+            <p>Bharathi Vidya Bhavan, Erode,TamilNadu</p>
+            <p>2022 – 2023</p>
+            <p>Percentage: 97%</p>
+          </p>
+        </div>
     </div>
   )
 }
